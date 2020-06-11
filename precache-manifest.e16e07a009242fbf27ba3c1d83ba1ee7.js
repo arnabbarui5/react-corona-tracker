66672ed2d310a24a6160ee2ae6b6cc0d2c968ad9
Arnab Barui <40000635+arnabbarui5@users.noreply.github.com>
2020-06-11 22:04:11 +0530

    Updates

---
+++ precache-manifest.e16e07a009242fbf27ba3c1d83ba1ee7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a17dbb579c43501a2a9912d28b90451",
+    "url": "/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "dc8668db524c8e040b11",
+    "url": "/react-corona-tracker/static/css/main.7643cfd3.chunk.css"
+  },
+  {
+    "revision": "b76aa54ba76f9a965e68",
+    "url": "/react-corona-tracker/static/js/2.bba27cce.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona-tracker/static/js/2.bba27cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8668db524c8e040b11",
+    "url": "/react-corona-tracker/static/js/main.21a9feba.chunk.js"
+  },
+  {
+    "revision": "599d240c9c1b73a3b3c0",
+    "url": "/react-corona-tracker/static/js/runtime-main.b76baf03.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona-tracker/static/media/image.d7265326.png"
+  }
+]);
